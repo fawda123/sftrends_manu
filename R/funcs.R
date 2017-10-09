26251c68f8377dd5cf1b1fa932195417af62b581
fawda123 <mbafs2012@gmail.com>
2017-10-09 10:48:31 -0700

    fixed table formats - one star for sig, no bold-italic

--- R/funcs.R
+++ R/funcs.R
@@ -29,8 +29,8 @@ p_form <- function(x, tex_out = F){
 # another stupid function for formatting p-values as asterisks
 p_ast <- function(x){
   
-  sig_cats <- c('**', '*', '')
-  sig_vals <- c(-Inf, 0.005, 0.05, Inf)
+  sig_cats <- c('*', '')
+  sig_vals <- c(-Inf, 0.05, Inf)
   
   out <- cut(x, breaks = sig_vals, labels = sig_cats, right = FALSE)
   out <- as.character(out)
